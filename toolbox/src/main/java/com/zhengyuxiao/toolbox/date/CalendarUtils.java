d0466a6a0586b3c406189aab6a0ec85781755393
xiaozhengyu <zhengyupython@qq.com>
2020-11-05 15:41:38 +0800

    1. 重写判断两个日期相差天数的方法 2. 重写判断两个日期是否是同一天的方法

--- toolbox/src/main/java/com/zhengyuxiao/toolbox/date/CalendarUtils.java
+++ toolbox/src/main/java/com/zhengyuxiao/toolbox/date/CalendarUtils.java
@@ -54,13 +54,50 @@ public class CalendarUtils {
     }
 
     /**
+     * 计算两个日期间相差的天数。
+     *
+     * @param date1 - 日期1
+     * @param date2 - 日期2
+     * @return - 两个日期相差的天数。注意，本方法认为 “2020-01-01 23:59:59” 与 “2020-01-02 00:00:00”相差1天。
+     */
+    public static long getDaysOfDifference(Date date1, Date date2) {
+        if (date1 == null || date2 == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(date1);
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(date2);
+        return getDaysOfDifference(cal1, cal2);
+    }
+
+    /**
+     * 计算两个日期间相差的天数。
+     *
+     * @param cal1 - 日期1
+     * @param cal2 - 日期2
+     * @return - 两个日期相差的天数。注意，本方法认为 “2020-01-01 23:59:59” 与 “2020-01-02 00:00:00”相差1天。
+     */
+    public static long getDaysOfDifference(Calendar cal1, Calendar cal2) {
+        if (cal1 == null || cal2 == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        cleanTime(cal1);
+        cleanTime(cal2);
+
+        long dayOfDifference = (cal1.getTimeInMillis() - cal2.getTimeInMillis()) / (1000 * 3600 * 24);
+        dayOfDifference = dayOfDifference < 0 ? -dayOfDifference : dayOfDifference;
+        return dayOfDifference;
+    }
+
+    /**
      * Returns whether the two dates are the same day.
      *
      * @param date1 the date 1
      * @param date2 the date 2
      * @return whether the two dates are the same day.
      */
-    public static boolean isTheSameDay(Date date1, Date date2) {
+    public static boolean isSameDay(Date date1, Date date2) {
         if (date1 == null || date2 == null) {
             throw new IllegalArgumentException("The date must not be null");
         }
@@ -71,6 +108,13 @@ public class CalendarUtils {
         return isSameDay(cal1, cal2);
     }
 
+    /**
+     * Returns whether the two dates are the same day.
+     *
+     * @param cal1 the date 1
+     * @param cal2 the date 2
+     * @return whether the two dates are the same day.
+     */
     public static boolean isSameDay(Calendar cal1, Calendar cal2) {
         if (cal1 == null || cal2 == null) {
             throw new IllegalArgumentException("The date must not be null");
@@ -79,5 +123,50 @@ public class CalendarUtils {
         return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA) &&
                 cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
                 cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR));
+    }
+
+    /**
+     * 清洗时间——将时、分、秒、毫秒清零
+     *
+     * @param calendar - 待处理的日期
+     * @return - 时、分、秒、毫秒被清零的日期
+     */
+    public static Calendar cleanTime(Calendar calendar) {
+        if (calendar == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        return calendar;
+    }
+
+    /**
+     * 清晰日期——将年、月、日清零
+     *
+     * @param calendar - 待处理的日期
+     * @return - 年、月、日被清零的日期
+     */
+    public static Calendar cleanDate(Calendar calendar) {
+        if (calendar == null) {
+            throw new IllegalArgumentException("The date must not be null");
+        }
+        calendar.set(Calendar.YEAR, 0);
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 0);
+        return calendar;
+    }
+
+    public static void main(String[] args) throws Exception {
+        // SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        // Date date1 = simpleDateFormat.parse("2020-01-01 23:59:59");
+        // Date date2 = simpleDateFormat.parse("2020-01-01 00:00:00");
+        // Date date3 = simpleDateFormat.parse("2020-01-02 00:00:00");
+
+        // System.out.println(getDaysOfDifference(date1, date2));
+        // System.out.println(getDaysOfDifference(date1, date3));
+        // System.out.println(getDaysOfDifference(date2, date3));
+
     }
 }
