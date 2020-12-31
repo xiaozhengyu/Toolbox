35c8f8a7d32eb1f59c5e3acff16e60d7f7b52743
xiaozhengyu <zhengyupython@qq.com>
2020-12-31 15:09:20 +0800

    isSameMonth

--- toolbox/src/main/java/com/zhengyuxiao/toolbox/date/CalendarUtils.java
+++ toolbox/src/main/java/com/zhengyuxiao/toolbox/date/CalendarUtils.java
@@ -196,9 +196,24 @@ public class CalendarUtils {
             throw new NullPointerException();
         }
 
-        return (cal1.get(Calendar.ERA) == cal2.get(Calendar.ERA) &&
-                cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+        return (cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH) &&
                 cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR));
+    }
+
+    public static boolean isSameMonth(Calendar cal1, Calendar cal2) {
+        if (cal1 == null || cal2 == null) {
+            throw new NullPointerException();
+        }
+
+        return cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.MONTH) == cal2.get(Calendar.MONTH);
+    }
+
+    public static boolean isSameMonth(Date date1, Date date2) {
+        if (date1 == null || date2 == null) {
+            throw new NullPointerException();
+        }
+        return isSameMonth(date2Calendar(date1), date2Calendar(date2));
     }
 
     /**
